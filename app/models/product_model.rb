459ecc20fdf5ba022c19f4246b736cbb3657bfc4
beelarr <bryonl@me.com>
2017-11-02 16:03:51 -0500

    Model inital build, no tests

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,5 +1,13 @@
-class Product
-  def create_new_product
+require_relative '../../app/controllers/product_controller'
 
+class Product
+  def create_new_product(hash_from_controller)
+    d = Date.now
+    date = "#{d.month}/#{d.day}/#{d.year}"
+    hash_from_controller[:created_at] = date
+    # hash_from_controller[:updated_at] = date
+    db = SQlite3::Database.open('../db/sprint2.sqlite')
+    db.execute(hash_from_controller)
+    db.close
   end
 end
