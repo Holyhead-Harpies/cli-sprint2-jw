a2f2590ac2116e393b5da4390880571f0b2810ee
beelarr <bryonl@me.com>
2017-11-03 09:36:31 -0500

    Add model and tests for model

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,13 +1,32 @@
 require_relative '../../app/controllers/product_controller'
+require 'date'
 
 class Product
+
+  def initialize
+    @db = SQLite3::Database.new('../db/sprint2.sqlite')
+    @db.results_as_hash = true
+  end
+
   def create_new_product(hash_from_controller)
-    d = Date.now
+    d = Datetime.now
     date = "#{d.month}/#{d.day}/#{d.year}"
     hash_from_controller[:created_at] = date
-    # hash_from_controller[:updated_at] = date
-    db = SQlite3::Database.open('../db/sprint2.sqlite')
-    db.execute(hash_from_controller)
-    db.close
+    hash_from_controller[:updated_at] = date
+    begin
+      statement = "INSERT INTO Products(Title, Description, Price, Quantity, created_at, updated_at) VALUES(
+                  '#{hash_from_controller[:title]}', '#{hash_from_controller[:description]}',
+                  '#{hash_from_controller[:price]}', '#{hash_from_controller[:quantity]}',
+                  '#{hash_from_controller[:created_at]}', '#{hash_from_controller[:updated_at]}'"
+      @db.transaction
+      @db.execute statement
+      @db.commit
+    rescue SQLite3::Exception => e
+      puts 'Exception occurred from ProductModel.create_new_product'
+      puts e
+      @db.rollback
+    ensure
+      @db.close
+    end
   end
 end
