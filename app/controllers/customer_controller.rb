1240af48667f2beb5b675c7dcdd269cfa52b5f9d
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-03 11:38:28 -0500

    Added to Customer Active Controller and Model

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,12 +1,35 @@
-require './app/models/customer_model.rb'
+require_relative "../../app/models/customer_model"
 
 class CustomerController
+
 	def initialize
 		@CustomerModel = CustomerModel.new
 	end
-	def select_customer
+
+
+	def show_all_customers
+		@customers = @CustomerModel.get_all_customers
+		@customers.each_with_index do |customer,index|
+			p "#{index+1}: #{customer['Name']}"
+		end
+		select_customer(@customers)
+	end
+
+	def select_customer(customers)
 		puts "Which Customer Will Be Active?"
 		customerid = STDIN.gets.chomp
-		@CustomerModel.select_customer(customerid)
+		set_customer(customerid,customers)
+	end
+
+	def set_customer(cid,customers)
+		if cid =~ /[^1-9]/ || cid < 1 || cid > customers.length
+			puts 'Customer Does Not Exist'
+			'Customer Does Not Exist'
+		else
+			cid
+		end
 	end
 end
+
+# customer = CustomerController.new
+# p customer.show_all_customers
--- app/models/customer_model.rb
+++ app/models/customer_model.rb
@@ -1,17 +1,16 @@
 require 'sqlite3'
+
 class CustomerModel
+
 	def initialize
 		@db = SQLite3::Database.open('./db/sprint2.sqlite')
 		@db.results_as_hash=true
 	end
-	def select_customer(customer)
-		stm = @db.execute "select * from Customers where CustomerId = #{customer}"
-		if stm == []
-			'Customer Does Not Exist'
-		else
-			stm[0]['CustomerId']
-		end
+
+	def get_all_customers
+		stm = @db.execute "select * from Customers"
 	end
+
 
 
 end
