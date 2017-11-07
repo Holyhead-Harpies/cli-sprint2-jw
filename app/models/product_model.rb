ed55974ef98674da1eb2ad5cf76f2b725a48e67f
beelarr <bryonl@me.com>
2017-11-07 12:26:49 -0600

    Chang sql query to pull the Title

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -158,12 +158,12 @@ class Product
     one_eighty_days_ago = (now - 180)
     ninety_days_ago = (now-90)
     begin
-      stale_products =  "SELECT Products.ProductId as Product from Products join OrdersProducts where Products.ProductId != OrdersProducts.ProductId and
+      stale_products =  "SELECT Products.Title as Product from Products join OrdersProducts where Products.ProductId != OrdersProducts.ProductId and
                             Products.created_at < #{one_eighty_days_ago}
                             UNION ALL
-                            SELECT  OrdersProducts.ProductId from OrdersProducts where created_at < #{ninety_days_ago}
+                            SELECT  Products.Title from Products join OrdersProducts.ProductId from OrdersProducts where created_at < #{ninety_days_ago}
                             UNION ALL
-                            SELECT  Products.ProductId from Products join Orders, OrdersProducts where Orders.completed == '1' and
+                            SELECT  Products.Title from Products join Orders, OrdersProducts where Orders.completed == '1' and
                             OrdersProducts.ProductId != null and Products.quantity > 0 and Products.created_at < #{one_eighty_days_ago}"
       @db.transaction
       @db.execute stale_products
