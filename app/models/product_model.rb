66f2b8bfe67cfa0d16f056076869d955637a1082
Michael Lindstrom <michaellindstr@gmail.com>
2017-11-08 10:04:26 -0600

    fixing merge

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -97,6 +97,7 @@ class ProductModel
 
   def get_product_price_info(product_id)
     begin 
+      @db = open_db_connection
       return @db.execute("select p.ProductId, p.Title, p.Price from Products p where p.ProductId = #{product_id}")
     rescue SQLite3::Exception => e
       @db.rollback
@@ -109,6 +110,7 @@ class ProductModel
   end
 
   def get_quantity(product)
+    @db = open_db_connection
     quantity = @db.execute("select p.Quantity from Products p where p.ProductId = #{product[0]}")
     return quantity
   end
@@ -116,6 +118,7 @@ class ProductModel
   def reduce_quantity(product)
     old_quantity = get_quantity(product)
     new_quantity = old_quantity[0][0] - product[:number_on_order]
+    @db = open_db_connection    
     @db.execute("update Products set Quantity = #{new_quantity} where ProductId = #{product[0]}")
     @db.close
   end
